1015cdef67f6882a63616e8cfa11b32f7f33ceab
Trynda-LOL <40008415+Trynda-LOL@users.noreply.github.com>
2020-08-18 15:24:02 -0300

    vuex

--- vuex/vuex-exercicios/src/components/Resumo.vue
+++ vuex/vuex-exercicios/src/components/Resumo.vue
@@ -12,17 +12,10 @@
 export default {
     computed: {
         total() {
-            return this.produtos.map(p => p.quantidade * p.preco)
-                .reduce((total, atual) => total + atual, 0)
-        }
-    },
-    data() {
-        return {
-            produtos: [
-                { nome: 'Produto 1', quantidade: 7, preco: 14.55 },
-                { nome: 'Produto 2', quantidade: 10, preco: 22.99 },
-                { nome: 'Produto 3', quantidade: 1, preco: 43.18 },
-            ]
+            return this.$store.getters.valorTotal
+        },
+        produtos() {
+            return this.$store.state.produtos
         }
     }
 }
